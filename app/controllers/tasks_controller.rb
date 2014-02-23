3cfb68c903854fc1080ae70be5b1fe03fc3edbb7
ars <arsemchik@gmail.com>
2014-02-23 20:50:01 +0300

    level 1 answer

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,28 @@
 class TasksController < ApplicationController
 
   ANSWER = 'снежные'.freeze
+  SERVER_URL = 'http://localhost:3000/quiz'.freeze
 
   def index
     @token = params[:token]
-    render text: ANSWER, :encoding => 'utf-8'
+    render text: ANSWER
   end
 
   def quiz
-    @level = params[:level]
-    @question = params[:question]
-  end
+    level = params[:level]
+    question = params[:question]
+    token = params[:token]
 
+    poem = Poem.where('content like ?', "%#{question}%").first
+
+    if poem
+      uri = URI(SERVER_URL)
+      request = Net::HTTP::Post.new(uri)
+      request.post_form(answer: poem.title, token: token, level: level)
+    end
+
+    render nothing: true
+
+  end
 
 end
